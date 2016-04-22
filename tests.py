8484e4275afd702ade2c5c62757cd07462ade25e
Hackbright Student <info@hackbrightacademy.com>
2016-04-22 13:02:16 -0700

    unit tests added

--- tests.py
+++ tests.py
@@ -15,21 +15,22 @@ class PartyTests(unittest.TestCase):
         self.assertIn("I'm having a party", result.data)
 
     def test_no_rsvp_yet(self):
-        # FIXME: Add a test to show we haven't RSVP'd yet
-        print "FIXME"
+        result = self.client.get("/")
+        self.assertIn("<h2>Please RSVP</h2>", result.data)
+        self.assertNotIn("<h2>Party Details</h2>", result.data)
 
     def test_rsvp(self):
         result = self.client.post("/rsvp",
                                   data={'name': "Jane", 'email': "jane@jane.com"},
                                   follow_redirects=True)
-        # FIXME: check that once we log in we see party details--but not the form!
-        print "FIXME"
+        self.assertNotIn("<h2>Please RSVP</h2>", result.data)
+        self.assertIn("<h2>Party Details</h2>", result.data)
 
     def test_rsvp_mel(self):
-        # FIXME: write a test that mel can't invite himself
-        pass
-        print "FIXME"
-
+        result = self.client.post("/rsvp",
+                                  data={'name': "Mel Melitpolski", 'email': "mel@ubermelon.com"},
+                                  follow_redirects=True)
+        self.assertNotIn("<h2>Party Details</h2>", result.data)
 
 if __name__ == "__main__":
     unittest.main()
